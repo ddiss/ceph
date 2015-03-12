645b6b073dad5e84b1f0f93419f360fa78bd7c7e
Zhiqiang Wang <zhiqiang.wang@intel.com>
2015-03-12 13:54:51 +0800

    osd: refactor the skip promotion logic
    Moving the skip promotion detection logic into init_op_flags, avoid
    doing extra checking.
    
    Signed-off-by: Zhiqiang Wang <zhiqiang.wang@intel.com>
    (cherry picked from commit d42c2c4517140619a0edb75c6ffb71a24372d4b0)

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -1792,13 +1792,17 @@ bool ReplicatedPG::maybe_handle_cache(OpRequestRef op,
   if (obc.get() && obc->obs.exists) {
     return false;
   }
-
+  
   MOSDOp *m = static_cast<MOSDOp*>(op->get_req());
   const object_locator_t& oloc = m->get_object_locator();
 
   if (must_promote || op->need_promote()) {
     promote_object(obc, missing_oid, oloc, op);
     return true;
+  }
+
+  if (op->need_skip_promote()) {
+    return false;
   }
 
   // older versions do not proxy the feature bits.
@@ -1824,9 +1828,7 @@ bool ReplicatedPG::maybe_handle_cache(OpRequestRef op,
       waiting_for_cache_not_full.push_back(op);
       return true;
     }
-    if (can_skip_promote(op)) {
-      return false;
-    }
+
     if (op->may_write() || write_ordered || !hit_set) {
       promote_object(obc, missing_oid, oloc, op);
       return true;
@@ -1905,9 +1907,6 @@ bool ReplicatedPG::maybe_handle_cache(OpRequestRef op,
 	waiting_for_cache_not_full.push_back(op);
 	return true;
       }
-      if (can_skip_promote(op)) {
-	return false;
-      }
       promote_object(obc, missing_oid, oloc, op);
       return true;
     }
@@ -1925,9 +1924,6 @@ bool ReplicatedPG::maybe_handle_cache(OpRequestRef op,
 	waiting_for_cache_not_full.push_back(op);
 	return true;
       }
-      if (can_skip_promote(op)) {
-	return false;
-      }
       promote_object(obc, missing_oid, oloc, op);
       return true;
     }
@@ -1939,20 +1935,6 @@ bool ReplicatedPG::maybe_handle_cache(OpRequestRef op,
   default:
     assert(0 == "unrecognized cache_mode");
   }
-  return false;
-}
-
-bool ReplicatedPG::can_skip_promote(OpRequestRef op)
-{
-  MOSDOp *m = static_cast<MOSDOp*>(op->get_req());
-  if (m->ops.empty())
-    return false;
-  // if we get a delete with FAILOK we can skip promote.  without
-  // FAILOK we still need to promote (or do something smarter) to
-  // determine whether to return ENOENT or 0.
-  if (m->ops[0].op.op == CEPH_OSD_OP_DELETE &&
-      (m->ops[0].op.flags & CEPH_OSD_OP_FLAG_FAILOK))
-    return true;
   return false;
 }
 
